a424c617f66939c348a86ddbaf90836905e0440e
Sky-Joke <guigui.douillon@gmail.com>
2025-12-14 20:20:39 +0100

    changement

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -31,8 +31,8 @@ export default defineConfig({
     sepolia: {
       type: "http",
       chainType: "l1",
-      url: configVariable("SEPOLIA_RPC_URL"),
-      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.SEPOLIA_PRIVATE_KEY ? [process.env.SEPOLIA_PRIVATE_KEY] : [],
     },
   },
 });
